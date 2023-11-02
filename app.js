770bdd9b83b4df780ff6f38bcb8a17e7f5cc0680
CDNJ <contact.cdnjmusic@gmail.com>
2023-11-02 17:01:14 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -25,3 +25,23 @@ arr.forEach(button => {
         
     })
 })
+
+window.addEventListener('keydown', function(e) {
+    let key = e.key;
+    if (key >= 0 && key <= 9) { // If the key is a number
+        string += key;
+        input.value = string;
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') { // If the key is an operator
+        string += key;
+        input.value = string;
+    } else if (key === 'Enter') { // If the key is Enter, calculate the result
+        string = eval(string);
+        input.value = string;
+    } else if (key === 'Backspace') { // If the key is Backspace, delete the last character
+        string = string.substring(0, string.length-1);
+        input.value = string;
+    } else if (key === 'Escape') { // If the key is Escape, clear all
+        string = "";
+        input.value = string;
+    }
+});
